e969f69f5b618e89bfcee8f3febf8904a1d2c6ec
Ananya Dhiman <ananya.dhiman@somaiya.edu>
2024-04-27 21:46:15 +0530

    read me file updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-a-Sketch
+# Link-https://ananya-dhiman.github.io/Etch-a-Sketch/
--- javascript.js
+++ javascript.js
@@ -1,11 +1,20 @@
 container=document.querySelector(".container");
 newGridBtn=document.querySelector(".new-grid-btn");
+let counter=0;
+function rc(){
+  var letters = '0123456789ABCDEF';
+  var random_color = '#';
+  for (var i = 0; i < 6; i++) {
+    random_color += letters[Math.floor(Math.random() * 16)];
+  }
+  return random_color;
 
+}
 function createGrid(no) {
   container.innerHTML="";
   container.style.display = 'flex'; // Ensures the container uses flexbox
   container.style.flexWrap = 'wrap'; // Allows items to wrap in the container
-  let w=parseInt(960/no);
+  let w=parseInt(Math.ceil(960/no));
   console.log(w);
   for (let i = 0; i < no * no; i++) {
     
@@ -20,21 +29,43 @@ function createSquare(w) {
   square.style.width=w + "px";
   square.style.height=w + "px";
   square.classList.add('square');
-  console.log("Hello");
+ 
   return square;
 }
 function startover(numSquaresPerSide){
   createGrid(parseInt(numSquaresPerSide));
-  console.log("Hello");
+ 
   container=document.querySelector(".container");
   const on = document.getElementsByClassName('square');
+
   Array.from(on).forEach(square=>{
-    square.style.backgroundColor = 'aqua';
+    console.log(rc());
+    square.dataset.number = 0;
+    let maxNumber=10;
+    
+    square.style.backgroundColor = 'white';
     square.addEventListener('mouseover', () => {
-      square.style.backgroundColor='blue';
-  
+      let n= parseInt(square.dataset.number );
+      
+      if(n<maxNumber){
+        n=n+1;
+        square.dataset.number= n;
+        counter=counter+1;
+        console.log(counter);
+        square.style.backgroundColor=rc();
+        square.style.opacity=(n*(0.1));
+      }
+      else if(n=>maxNumber){
+        square.style.backgroundColor="black";
+      };
+
+      
 })
+    
+    
 });
+
+
   
 
 };
